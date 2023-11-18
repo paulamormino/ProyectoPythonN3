2cb3495f6ec6b7302d330a1c0538fd34648330c6
Paula Mormino <paulamormino@hotmail.com>
2023-11-18 18:16:18 -0300

    correcciones 2.0

--- mywebproject/myapp/urls.py
+++ mywebproject/myapp/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('listar_productos/', views.listar_productos, name='listar_productos'),
     path('crear_producto/', views.crear_producto, name='crear_producto'),
     path('', views.inicio, name='inicio'),
+    path("", include("myapp.urls")),
 ]
